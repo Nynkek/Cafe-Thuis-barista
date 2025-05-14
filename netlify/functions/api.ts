6f57f8bb63c8a35742a8aeebca183188d1ee0310
Nynke <35605149+Nynkek@users.noreply.github.com>
2025-05-14 15:09:21 +0200

    test api call

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,4 +1,10 @@
-import serverless from "serverless-http";
-import { app } from "../../backend/src"; 
+import serverless from 'serverless-http';
+import express from 'express';
+
+const app = express();
+
+app.get('/recipes', (req, res) => {
+  res.json({ recipes: ['Pizza', 'Pasta'] });
+});
 
 export const handler = serverless(app);
